39e234aab1c74450299b3601eaa70f3a9843c521
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-03-22 14:30:30 -0300

    chore: criando toda eslitização da pagina de login

--- src/pages/Auth/SignIn/index.js
+++ src/pages/Auth/SignIn/index.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import { Container, SignForm, Image } from '../styles';
+import Button from '~/styles/components/Button';
+import Logo from '~/assets/discord.png';
 
-export default function SignIn() {
-  return (
-    <div />
-  );
+export default class SignIn extends Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = this.state;
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+
+    // logar
+  }
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <Container>
+        <SignForm onSubmit={this.handleSubmit}>
+          <Image src={Logo} alt="logo" />
+          <h1>Boas vindas!</h1>
+          <span>E-MAIL</span>
+          <input type="email" name="email" onChange={this.handleInputChange} value={email} />
+          <span>SENHA</span>
+          <input type="password" name="password" onChange={this.handleInputChange} value={password} />
+          <Button size="big" type="submit">Entrar</Button>
+        </SignForm>
+      </Container>
+    );
+  }
 }
