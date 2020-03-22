126fc5e4e278b4b8096b118d347271705acc084b
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-03-22 10:24:43 -0300

    chore: add style component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+
+import { Provider } from 'react-redux';
+import Global from 'styles/global';
+import store from '~/store';
+
 import Routes from '~/routes/index';
 
 const App = () => (
-  <Routes />
+  <Provider store={store}>
+    <>
+      <Routes />
+      <Global />
+    </>
+  </Provider>
 );
 
 export default App;
